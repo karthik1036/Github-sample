e82bf041a7134a33464cef39d8ea592051c4a6a9
hari teja hari <haritejahari@outlook.com>
2021-02-24 22:50:00 +0530

    Last change

---
+++ Github sample 2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Github_sample_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
